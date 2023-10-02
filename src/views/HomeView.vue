64b2947167cd0bc98f2c4218fadac0866f723b8e
bjgakress <bjgakress@gmail.com>
2023-10-02 17:11:10 +0200

    Display of Metadata expanded

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,44 @@
 <template>
   <main>
-    BPF main page
-  </main>
+    <p>Step 1</p>
+        <datalist id="current_processes">
+          <option value = "Here should"></option>
+          <option value = "be a list of"></option>
+          <option value = "all running ingest"></option>
+          <option value = "processes, but"></option>
+          <option value = "they must come"></option>
+          <option value = "from the back-end"></option>
+        </datalist>
+        <h1>Ingest Module</h1>
+        <h2>Start New Ingest Process - Choose Material</h2>
+        <div>
+          <form>
+            <input type="radio" id="manuscript" name="ingest_option" value="manuscript">
+            <label for="manuscript"> Import digitised manuscript</label><br>
+            <input type="radio" id="book" name="ingest_option" value="book">
+            <label for="book"> Import digitised book</label><br>
+            <input type="radio" id="museum" name="ingest_option" value="museum" disabled>
+            <label for="museum"> Import museum objects (to be added later)</label><br>
+            <input type="radio" id="harddrive" name = "ingest_option" value ="harddrive" disabled>
+            <label for="harddrive"> Import images from harddrive (to be added later)</label><br>
+
+
+            <label for="ingest_id_new">Enter Ingest ID (leave blank for default ID): </label>
+            <input type="text" id="ingest_id_new" name="ingest_id_new">
+          </form>
+        </div>
+        <br>
+        <br>
+        <div>
+          <form>
+            <h2>Reopen Existing Ingest Process</h2>
+            <p> Please choose Ingest Process ID from List</p>
+            <input list = "current_processes" id="choose_process" name="choose_process">
+
+          </form>
+        </div>
+
+</main>
 </template>
 
 
--- src/views/NewRessource.vue
+++ src/views/NewRessource.vue
@@ -1,35 +1,84 @@
 <template>
-    <div>
+   <!----
+           <p>Step 2 (Books)</p>
+        <p>Ingest ID: <samp>ingest_id</samp></p>
+         <h1>Ingest Module</h1>
+        <h2>Enter Book ID</h2>
+        
+        <form>
+            <label for="manifest">Enter IIIF manifest of manuscript: </label>
+            <input type="URL" id="manifest" name="manifest">
+            <label for="range"> Canvasses: </label>
+            <input type="text" id="range" name="range">
+            <label for="bibliographic_ID">Additional bibliographic record: </label>
+            <input type ="text" id="bibliographic_ID" name="bibliographic_ID">
+            
+        
+        <br>
+        <p> Separate canvas numbers by ";" or "-" for ranges (e.g., "1-10;350-370")</p>
+        <p> If no canvas numbers are given, all canvasses will be downloaded and analysed</p>
+        <br>
+        <p> If the IIIF manifest does contain bibliographic references (e.g. to the GW, VD16 etc.), you can add a reference here.</p>
+        <p> It should consist of the name of the bibliography and the ID of the book, e.g. "VD16 H 1234" or "GW M40784. </p>
+        <p> This information will supersede any bibliographical reference in the IIIF manifest.</p>
+        <input type="button" value="Check and complete catalogue records">
+        </form>
+
+-->
+<div>
         <h1>Create new ressource</h1>
     </div>
     <div>
         iiif manifest url <input v-model="iiifUrl" /><br>
-        VD16-Nummer <input v-model="vd16" /><br>
+        
 
         <button @click="getMetadata()">
             Get Metadata
         </button>
 
     </div>
+
+   <!----
+        <button @click="getMetadata()">
+            Start Download and Analysis
+        </button>
+    -->     
+    
     <h3>Metadata:</h3>
-    <v-table>
-        <tr v-if="metadata">
+    <v-table v-if="metadata">
+        <tr v-if="metadata.bibliographic_information[0]">
             <td>Title:</td>
-            <td>{{ metadata.title }}</td>
+            <td>{{ metadata.bibliographic_information[0].title }}</td>
         </tr>
-        <tr v-if="metadata">
-            <td>Location:</td>
-            <td>{{ metadata.location }}</td>
+        <tr v-if="metadata.bibliographic_information[0]" v-for="(person, index) in metadata.bibliographic_information[0].persons" :key="index">
+            <td>Person:</td>  
+            <td :key="indexColumn">{{person["name"]}}</td>
+            <td :key="indexColumn">{{person["id"]}}</td>
+            <td :key="indexColumn">{{ person["role"] }}</td>
+        </tr>      
+        <tr v-if="metadata.bibliographic_information[0]" v-for="(place, index) in metadata.bibliographic_information[0].places" :key="index">
+            <td>Place:</td>  
+            <td :key="indexColumn">{{place["name"]}}</td>
+            <td :key="indexColumn">{{place["id"]}}</td>
+            <td :key="indexColumn">{{place["role"] }}</td>
+        </tr>      
+        <tr>
+            <td>Repository:</td>
+            <td>{{ metadata.repository }}</td>
         </tr>
-        <tr v-if="metadata">
+        <tr>
+            <td> Shelfmark:</td>
+            <td> {{ metadata.shelfmark }}</td>
+        </tr>
+        <tr>
             <td>Number of Images:</td>
             <td>{{ metadata.numberOfImages }}</td>
         </tr>
-        <tr v-if="metadata">
+        <tr>
             <td>MARCXML:</td>
             <td><a :href="metadata.markxml" target="_blank">{{ metadata.markxml }}</a></td>
         </tr>
-        <tr v-if="metadata">
+        <tr>
             <td>IIIF URL</td>
             <td><a :href="metadata.iiifUrl" target="_blank">{{ metadata.iiifUrl }}</a></td>
         </tr>
@@ -37,6 +86,7 @@
     <button @click="$event => createNewRessource(metadata)">
         Create new ressource
     </button>
+
 </template>
 // https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00087481/manifest
 
@@ -59,6 +109,7 @@ export default {
         return {
             iiifurl: '',
             vd16: '',
+            vd17: '',
             message: '',
             metadata: ''
         };
@@ -67,6 +118,7 @@ export default {
         getMetadata() {
             const url = `${API_URL}/getMetadata`
             console.log(url)
+            console.log(this.vd17)
             return axios.get(url,
                 {
                     params: {
